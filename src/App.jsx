40c0a8c0f3637ef00ea8a55b31e8520317a58035
Alex G <62220408+poplizards@users.noreply.github.com>
2026-03-11 09:14:40 -0700

    Create basic operating system

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,7 @@
-import { useState } from 'react'
-import './App.css'
+import OS from './os/OS'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <h1>TattleTale</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-    </>
-  )
+  return <OS />
 }
 
 export default App
